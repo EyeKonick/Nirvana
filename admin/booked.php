3de0bcc8a723b7af970b4a4cf739daf0620a674a
jjcalovera <jjcalovera@gmail.com>
2024-02-12 14:45:09 +0800

    remove ed from confirmed and checked out

--- admin/booked.php
+++ admin/booked.php
@@ -145,14 +145,14 @@ try {
                   <?php if ($booking->isConfirmed == false) : ?>
                     <a href="confirm_confirm.php?id=<?= $booking->id; ?>" class="btn btn-success w-100">Confirm</a>
                   <?php else : ?>
-                    <a href="#" class="btn btn-success disabled w-100">Confirmed</a>
+                    <a href="#" class="btn btn-success disabled w-100">Confirm</a>
                   <?php endif; ?>
                 </div>
                 <div class="col">
                   <?php if ($booking->isBooked == true) : ?>
                     <a href="confirm_check_out.php?id=<?= $booking->id; ?>" class="btn btn-warning w-100">Check Out</a>
                   <?php else : ?>
-                    <a href="#" class="btn btn-warning disabled w-100">Checked Out</a>
+                    <a href="#" class="btn btn-warning disabled w-100">Check Out</a>
                   <?php endif; ?>
                 </div>
                 <div class="col">
